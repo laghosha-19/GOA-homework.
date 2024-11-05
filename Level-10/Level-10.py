a5027fd55f987eccb4361faa8b04d27b5e74feba
laghosha-19 <lagoshvili.luka@mail.ru>
2024-11-05 22:54:39 +0400

    level-10

---
+++ Level-10/Level-10.py
@@ -0,0 +1,54 @@
+num1 = int(input(" Your age "))
+print(type(num1))
+print(num1)
+num2 = int(input("Amount of money "))
+print(type(num2))
+print(num2)
+num3 = float(input("KG "))
+print(type(num3))
+print(num3)
+
+
+print("Calculating BMI")
+height = float(input("Your height "))
+weight = float(input("Your weight "))
+BMI = Weight / Height ** 2 
+print(BMI)
+
+#>, <, >=, <=, ==, !=
+#>
+print(10 > 5)
+print(10 > 1)
+print(10 > 2)
+print(10 > 11)
+print(10 > 7)
+#<
+print(100 < 150)
+print(100 < 140)
+print(100 < 122)
+print(100 < 155)
+print(100 < 177)
+#>=
+print(55 >= 55)
+print(66 >= 22)
+print(20 >= 60)
+print(77 >= 10)
+print(200 >= 100)
+#<=
+print(200 <= 200)
+print(100 <= 200)
+print(150 <= 160)
+print(100 <= 100)
+print(177 <= 178)
+#==
+print(2000 == 2000)
+print(4000 == 4000)
+print(7000 == 7000)
+print(56000 == 22000)
+print(22000 == 111000)
+#!=
+print(200 != 400)
+print(100 != 400)
+print(150 != 400)
+print(111 != 222)
+print(1111 != 2222)
