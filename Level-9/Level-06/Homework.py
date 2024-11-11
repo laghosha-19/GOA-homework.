8f54a0fe86b864b04df7bcd2bf8358cd2b357801
laghosha-19 <lagoshvili.luka@mail.ru>
2024-11-11 22:12:28 +0400

    Level-10

---
+++ Level-9/Level-06/Homework.py
@@ -0,0 +1,245 @@
+#პროგრამირებაში არსებობს case sensitive - case insensitive-ბი 
+#ase sensitive-ბია JavaScript-C++-Python
+#case insensitive-ია HTML
+#case sensitive როგორც ვიცით ინგლისურ ენაში გვაქვს დიდი და პატარა
+#ასოები კოდის წერისას ზემოთ აღნიშნულ პროგრამებში დიდი და პატარა
+#ასოები განსხვავდება ერთმანეთისგან
+#case insensitive-როდესაც არ აქვს აზრი დავწერთ თუარა კოდში დიდ ან პატარა ასოებს 
+#ის მაინც გამოიტანს კოდს error-ის გარეშე
+
+#debugging-ნიშნავს როდესაც კოდში აღმოვაჩენთ error-ს და გამოვასწორებთ მას ეს ბუნებრივია 
+#როდესაც დავუშვებთ შეცდომას მთავარია ვეცადოთ მის გამოსწორებას
+
+
+num1 = 100
+num1 = num1 + 120
+print (num1)
+
+num1 = 150
+num1 = num1 + 150
+print (num1)
+
+num1 = 200
+num1 = num1 + 170
+print (num1)
+
+num1 = 400
+num1 = num1 + 100
+print (num1)
+
+num1 = 600
+num1 = num1 + 220
+print (num1)
+
+num1 = 340
+num1 = num1 + 160
+print (num1)
+
+num1 = 400
+num1 = num1 + 400
+print (num1)
+
+num1 = 1000
+num1 = num1 + 1500
+print (num1)
+
+num1 = 5000
+num1 = num1 + 6666
+print (num1)
+
+num1 = 1111
+num1 = num1 + 22222
+print (num1)
+
+#this is deduction
+num2 = 2500
+num2 = num2 - 500
+print (num2)
+
+num2 = 4360
+num2 = num2 - 2153
+print (num2)
+
+num2 = 6653
+num2 = num2 - 3221
+print (num2)
+
+num2 = 9999
+num2 = num2 - 9998
+print (num2)
+
+num2 = 10000
+num2 = num2 - 9999
+print (num2)
+
+#this is devide
+num3 = 500
+num3 = num3 / 100
+print(num3)
+
+num3 = 1000
+num3 = num3 / 100
+print(num3)
+
+num3 = 6000
+num3 = num3 / 3
+print(num3)
+
+num3 = 3333
+num3 = num3 / 111
+print(num3)
+
+num3 = 2222
+num3 = num3 / 1111
+print(num3)
+
+num3 = 2500
+num3 = num3 / 50
+print(num3)
+
+num3 = 50000
+num3 = num3 / 250
+print(num3)
+
+num3 = 3500
+num3 = num3 / 500
+print(num3)
+
+num3 = 1200
+num3 = num3 / 6
+print(num3)
+
+num3 = 400
+num3 = num3 / 200
+print(num3)
+
+#this is multiply
+num4 = 1000
+num4 = num4 * 100
+print (num4)
+
+num4 = 10
+num4 = num4 * 100
+print (num4)
+
+num4 = 2000
+num4 = num4 * 200
+print (num4)
+
+num4 = 4000
+num4 = num4 * 150
+print (num4)
+
+num4 = 6666
+num4 = num4 * 1111
+print (num4)
+
+num4 = 7777
+num4 = num4 * 777
+print (num4)
+
+num4 = 2241
+num4 = num4 * 412
+print (num4)
+
+num4 = 5344
+num4 = num4 * 554
+print (num4)
+
+num4 = 2235
+num4 = num4 * 756
+print (num4)
+
+num4 = 1243
+num4 = num4 * 1234
+print (num4)
+
+#this is residual devide
+num5 = 100
+num5 = num5 % 6
+print(num5)
+
+num5 = 250
+num5 = num5 % 7
+print(num5)
+
+num5 = 2232
+num5 = num5 % 145
+print(num5)
+
+num5 = 1200
+num5 = num5 % 111
+print(num5)
+
+num5 = 5534
+num5 = num5 % 1476
+print(num5)
+
+num5 = 6621
+num5 = num5 % 523
+print(num5)
+
+num5 = 4743
+num5 = num5 % 2121
+print(num5)
+
+num5 = 20204
+num5 = num5 % 2378
+print(num5)
+
+num5 = 2024
+num5 = num5 % 164
+print(num5)
+
+num5 = 1700
+num5 = num5 % 56
+print(num5)
+
+num5 = 1789
+num5 = num5 % 53
+print(num5)
+
+#this is sorting
+num6 = 10
+num6 = num6 ** 10
+print(num6)
+
+num6 = 5
+num6 = num6 ** 2
+print(num6)
+
+num6 = 7
+num6 = num6 ** 3
+print(num6)
+
+num6 = 22
+num6 = num6 ** 5
+print(num6)
+
+num6 = 200
+num6 = num6 ** 30
+print(num6)
+
+num6 = 150
+num6 = num6 ** 2
+print(num6)
+
+num6 = 30
+num6 = num6 ** 3
+print(num6)
+
+num6 = 6
+num6 = num6 ** 6
+print(num6)
+
+num6 = 78
+num6 = num6 ** 2
+print(num6)
+
+num6 = 42
+num6 = num6 ** 22
+print(num6)
+
+num6 = 11
+num6 = num6 ** 11
+print(num6)
